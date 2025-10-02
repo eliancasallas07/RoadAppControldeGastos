87b50966c9e7264f82888101b5c789553791566c
Elian Casallas <elian.casallas07@gmail.com>
2025-10-01 19:59:28 -0500

    Mejoras de estilos globales, formularios unificados y botones responsivos en móvil

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,6 +1,7 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'frontend/config/environment';
 
+
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
